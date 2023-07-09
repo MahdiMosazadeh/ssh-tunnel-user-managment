55557b5a58cca7692e148c16385c6fedfb95d506
Mahdi Mosazadeh <89032241+MahdiMosazadeh@users.noreply.github.com>
2023-07-09 15:50:15 +0330

    Update ssh-tunnel-users-managment.sh
    improve UI & add exit command .

--- ssh-tunnel-users-managment.sh
+++ ssh-tunnel-users-managment.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #Auther "Mahdi Mosazadeh"
 #Telegram -> @persvens
-#Github : https://github.com/MahdiMosazadeh
+#Github : https://github.com/MahdiMosazadeh/ssh-tunnel-user-managment
+while [ 1 ]
+do
 clear
 echo "**************************************"
 echo "         SSH User Management"
@@ -24,6 +26,9 @@ echo " 9 : Show Users Traffic Usage"
 echo " 10 : Set Traffic Limit For Specific User"
 echo " 11 : Set Time Limit For Specific User"
 echo ""
+echo "Exit :"
+echo " 0 : Quit Script !"
+echo ""
 
 while [ 1 ]
 do
@@ -32,39 +37,70 @@ do
         echo ""
                 if [ $operator == 1 ]
                 then
+                      clear
                       read -p "Enter Username :" userName
                       read -p "Enter Password :" -s passWord
                       useradd -p $(openssl passwd -1 $passWord) $userName
                       echo "User Is Added ."
+                      read -p "Enter Any Key To Back :" fakedata
+                      break
                 elif [ $operator == 2 ]
                 then
+                      clear
+                      echo "This Is All Users You Have :"
+                      cat /etc/shadow | grep '^[^:]*:[^\*!]' | cut -d : -f 1
                       read -p "Enter Username For Delete :" deleteUser
                       deluser $deleteUser
+                      read -p "Enter Any Key To Back :" fakedata
+                      break
                 elif [ $operator == 3 ]
                 then
+                      clear
+                      echo "All Users :"
                       cat /etc/shadow | grep '^[^:]*:[^\*!]' | cut -d : -f 1
-
+                      read -p "Enter Any Key To Back :" fakedata
+                      break
                 elif [ $operator == 4 ]
                 then
-		      read -p "Enter UserName You Want :" userNameSpecific
+                      clear
+                	    read -p "Enter UserName You Want :" userNameSpecific
                       cat /etc/passwd | grep $userNameSpecific
+                      read -p "Enter Any Key To Back :" fakedata
+                      break
                 elif [ $operator == 5 ]
                 then
+                      clear
                       echo "The Number Of Users is : "
                       cat /etc/shadow | grep '^[^:]*:[^\*!]' -c
+                      read -p "Enter Any Key To Back :" fakedata
+                      break
                 elif [ $operator == 6 ]
                 then
-                     echo "Online Users :"
-                     ps aux | grep ssh | grep -Eo '^[^ ]+' | sort | uniq
+                      clear
+                      echo "Online Users :"
+                      ps aux | grep ssh | grep -Eo '^[^ ]+' | sort | uniq
+                      read -p "Enter Any Key To Back :" fakedata
+                      break
                 elif [ $operator == 7 ]
                 then
-                     echo "Online SSH Connections :"
-                     ps aux | grep ssh
+                      clear
+                      echo "Online SSH Connections :"
+                      ps aux | grep ssh
+                      read -p "Enter Any Key To Back :" fakedata
+                      break
                 elif [ $operator == 8 ]
                 then
-                    read -p "Enter Your Username You Want :" userOnline
-                    ps aux | grep ssh | grep $userOnline
+                      clear
+                      read -p "Enter Your Username You Want :" userOnline
+                      ps aux | grep ssh | grep $userOnline
+                      read -p "Enter Any Key To Back :" fakedata
+                      break
+                elif [ $operator == 0 ]
+                then
+                      echo "** Bye Bye **"
+                      exit
                 else
-                        echo "Select Some Operator From Above List :"
+                      break
                 fi
+done
 done
